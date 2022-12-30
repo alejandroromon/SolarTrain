21a5cebc9e1d2c3fb534cc140b42dc0fbbf708e3
alejandroromon <alejandroromeromontero@gmail.com>
2022-12-30 12:21:29 +0000

    Cards + modal para recoger informacion

---
+++ src/js/component/CustomerForm.jsx
@@ -0,0 +1,118 @@
+import React from "react";
+
+function CustomerForm() {
+
+
+  return (
+    <div className="text-dark rounded-3">
+      <button
+        type="button"
+        className="btn btn-success mb-3 mt-3"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
+        data-bs-whatever="@mdo"
+      >
+        {" "}
+        I want to go!{" "}
+      </button>
+
+      <div
+        className="modal fade"
+        id="exampleModal"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">
+                Let's make your boarding faster
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <form>
+                <div className="mb-3">
+                  <label for="recipient-name" className="col-form-label">
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="recipient-name"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label for="recipient-surname" className="col-form-label">
+                    Surname
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="recipient-surname"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label for="recipient-email" className="col-form-label">
+                    Email
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="recipient-email"
+                  />
+                </div>
+                <div class="form-check">
+                  <input
+                    class="form-check-input"
+                    type="checkbox"
+                    value=""
+                    id="flexCheckDefault"
+                  />
+                  <label class="form-check-label" for="flexCheckDefault">
+                    I would like travel in business class
+                  </label>
+                </div>
+                <div class="form-check">
+                  <input
+                    class="form-check-input"
+                    type="checkbox"
+                    value=""
+                    id="flexCheckDefault"
+                  />
+                  <label class="form-check-label" for="flexCheckDefault">
+                    I would like to have a menu on board
+                  </label>
+                </div>
+                <div class="form-check">
+                  <input
+                    class="form-check-input"
+                    type="checkbox"
+                    value=""
+                    id="flexCheckDefault"
+                  />
+                  <label class="form-check-label" for="flexCheckDefault">
+                    I would like to sit in a quiet wagon
+                  </label>
+                </div>
+              </form>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-success mb-3 mt-3">
+                Book journey
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CustomerForm;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import trains from "../../train_results.json";
+import CustomerFormModal from './CustomerForm.jsx';
 
 const SearchingForm = () => {
-  const [date, setDate] = useState(new Date().toISOString());
+  const [date, setDate] = useState(new Date());
   const [passengers, setPassengers] = useState(1);
   const [departure, setDeparture] = useState("");
   const [trips, setTrips] = useState([])
@@ -10,27 +11,25 @@ const SearchingForm = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
 
+    console.log("SIUUUUU", new Date());
+    console.log(date);
 
+    // Filters to get an array of the next 3 available trips based on the customer search
     const filterDeparture = trains.filter((train) => train.origin === departure);
-
     const filterDate = filterDeparture.filter((train) => new Date(train.date_time_depart) >= new Date(date).getTime()).slice(0, 3);
-    console.log(filterDate);
     setTrips(filterDate);
-
   };
 
   return (
     // Here the customer will select preferences to search for a train
-    <div className="bg-dark col-3 rounded-3 text-light mx-3 my-3">
-      <form className="ms-3 pb-3" onSubmit={handleSubmit}>
-        <h2>Where are we going today?</h2>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-6 rounded-3 bg-dark text-light">
+      <form className="ms-3 pb-3 mt-3 pt-3" onSubmit={handleSubmit}>
+        <h2>Ready for a new Adventure?</h2>
         <label>
-          Departure:
-          <select
-            className=""
-            defaultValue={"Select a departure"}
-            onChange={(event) => setDeparture(event.target.value)}
-          >
+        <h5>Where is your journey starting?</h5>
+          <select className="" defaultValue={"Select a departure"} onChange={(event) => setDeparture(event.target.value)}>
             <option disabled>Select a departure</option>
             <option value="Paris">Paris</option>
             <option value="London">London</option>
@@ -38,47 +37,43 @@ const SearchingForm = () => {
         </label>
         <br />
         <label>
-          When would you like to travel?
-          <input
-            type="datetime-local"
-            value={date}
-            onChange={(event) => setDate(event.target.value)}
-            min={new Date()}
-          />
+          <h5>When would you like to travel?</h5>
+          <input type="datetime-local" value={date} min={new Date()} onChange={(event) => setDate(event.target.value)}/>
         </label>
         <br />
-        <label>
-          How many people is travelling?
-          <input
-            type="number"
-            value={passengers}
-            onChange={(event) => setPassengers(event.target.value)}
-            min={1}
-          />
-        </label>
+          <h5>How many people is travelling?</h5>
+          <input type="number" value={passengers} min={1} onChange={(event) => setPassengers(event.target.value)} />
         <br />
-        <button type="submit" className="btn btn-success mb-3">
+        <button type="submit" className="btn btn-success mb-3 mt-3">
           Search my trip
         </button>
       </form>
+      </div>
 
-      {/* Once clicked on the search button will appear cards with the next 3 trains that fits with the origin, time_depart and date_time_depart*/}
+      <div className="col-6 bg-dark">
+      Aqui va una imagen de una polla
+      </div>
+      </div>
 
-      {trips.map((train, i) =>
-        (
-          <div key={i}>
+
+      <div className="col-6 d-flex">
+        {trips.map((train, i) => (
+          <div key={i} className="bg-dark m-3 col-3 text-light rounded-3 ms-3 pb-3 mt-3 pt-3">
             <div>Date of depart: {train.date}</div>
             <div>Depart time: {train.time_depart}</div>
             <div>Arrival time: {train.time_arrival}</div>
             <div>Destination: {train.destination}</div>
-            <div>Available seats: {train.available_seats >= passengers ? train.available_seats : window.alert(`"Sorry, there are ${train.available_seats} free places. Please try with less travellers or in a different date"`)}</div>
+            <div>Available seats: {train.available_seats >= passengers ? train.available_seats : `"Sorry, there are ${train.available_seats} free place/s. Please try with less travellers or in a different date"`}</div>
+            <CustomerFormModal />
           </div>
         )
       )}
+      </div>
 
-      {/* Once clicked on Book now will appear a modal where we will ask for Name, Surname and email in order to add the feature of sending a confirmation number*/}
     </div>
   );
 };
 
 export default SearchingForm;
+{/* Once clicked on the search button will appear cards with the next 3 trains that fits with the origin, time_depart and date_time_depart */}
+{/* Once clicked on Book now will appear a modal where we will ask for Name, Surname and email in order to add the feature of sending a confirmation number*/}
